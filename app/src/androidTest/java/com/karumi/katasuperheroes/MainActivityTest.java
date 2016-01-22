518433fa8804e4bf987eacfec8f456a02fb309e1
Bernat Borrás Paronella <bernatbor15@gmail.com>
2016-01-22 11:40:19 +0100

    add single item test

--- app/src/androidTest/java/com/karumi/katasuperheroes/MainActivityTest.java
+++ app/src/androidTest/java/com/karumi/katasuperheroes/MainActivityTest.java
@@ -20,25 +20,35 @@ import android.support.test.InstrumentationRegistry;
 import android.support.test.espresso.intent.rule.IntentsTestRule;
 import android.support.test.runner.AndroidJUnit4;
 import android.test.suitebuilder.annotation.LargeTest;
+
 import com.karumi.katasuperheroes.di.MainComponent;
 import com.karumi.katasuperheroes.di.MainModule;
 import com.karumi.katasuperheroes.model.SuperHero;
 import com.karumi.katasuperheroes.model.SuperHeroesRepository;
 import com.karumi.katasuperheroes.ui.view.MainActivity;
-import it.cosenonjaviste.daggermock.DaggerMockRule;
-import java.util.Collections;
+
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import it.cosenonjaviste.daggermock.DaggerMockRule;
+
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static com.karumi.katasuperheroes.matchers.RecyclerViewItemsCountMatcher.recyclerViewHasItemCount;
 import static org.mockito.Mockito.when;
 
 @RunWith(AndroidJUnit4.class) @LargeTest public class MainActivityTest {
+
+  private static final int SINGLE_NUMBER_OF_SUPER_HEROES = 1;
 
   @Rule public DaggerMockRule<MainComponent> daggerRule =
       new DaggerMockRule<>(MainComponent.class, new MainModule()).set(
@@ -65,8 +75,37 @@ import static org.mockito.Mockito.when;
     onView(withText("¯\\_(ツ)_/¯")).check(matches(isDisplayed()));
   }
 
+  @Test public void checkOneHeroIsOnList() {
+    givenOneSuperHero();
+
+    startActivity();
+
+    onView(withId(R.id.recycler_view)).check(matches(recyclerViewHasItemCount(1)));
+  }
+
   private void givenThereAreNoSuperHeroes() {
     when(repository.getAll()).thenReturn(Collections.<SuperHero>emptyList());
+  }
+
+  private void givenOneSuperHero() {
+    generateSuperHeros(SINGLE_NUMBER_OF_SUPER_HEROES);
+  }
+
+  private List<SuperHero> generateSuperHeros(int numberOfSuperHeroes) {
+    List<SuperHero> superHeroes = new ArrayList<>();
+
+    for (int i = 0; i < numberOfSuperHeroes; i++) {
+      String superHeroName = "SuperHero - " + i;
+      String superHeroPhoto = "https://i.annihil.us/u/prod/marvel/i/mg/c/60/55b6a28ef24fa.jpg";
+      String superHeroDescription = "Description Super Hero - " + i;
+      SuperHero superHero =
+              new SuperHero(superHeroName, superHeroPhoto, false, superHeroDescription);
+      superHeroes.add(superHero);
+      when(repository.getByName(superHeroName)).thenReturn(superHero);
+    }
+    when(repository.getAll()).thenReturn(superHeroes);
+
+    return superHeroes;
   }
 
   private MainActivity startActivity() {
